6159e9938a887a33f249cf3d2c614777029f18b9
986532qw <986532qw@gmail.com>
2021-03-23 21:49:51 +0300

    Initial commit

---
+++ TabbedApp/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  TabbedApp
+//
+//  Created by farkhod on 23.03.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
